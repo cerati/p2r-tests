daf90eacfa3cd068a151710f91e26c7e152f1306
alexstrel <astrel@fnal.gov>
2022-03-12 23:27:07 -0600

    added optimized cuda version added pstl version with native ordering added (renamed) optimized pstl version

---
+++ build-pstl-native.sh
@@ -0,0 +1,1 @@
+nvc++ -Iinclude -O2 -std=c++17 -stdpar=gpu -gpu=cc86 -gpu=managed -gpu=fma -gpu=fastmath -gpu=autocollapse -gpu=loadcache:L1 -gpu=unroll -o propagate-tor-test_pstl_cuda_nvc++_N5_native src/propagate-tor-test_pstl_native.cpp -Dntrks=8192 -Dnevts=100 -DNITER=5 -Dbsize=1 -Dnlayer=20
